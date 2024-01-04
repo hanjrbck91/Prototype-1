cb6b814d7489d2e606c4fccb2616629c0411e7cf
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2024-01-04 09:47:41 +0530

    challenge completed

--- Assets/Challenge 1/Scripts/RotatePropellyerX.cs
+++ Assets/Challenge 1/Scripts/RotatePropellyerX.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class RotatePropellyerX : MonoBehaviour
 {
-    public GameObject plane;
-    public Vector3 offset;
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
     }
 }
