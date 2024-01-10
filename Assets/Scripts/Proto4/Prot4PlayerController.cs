56ad3a326edf90dcfd959cdc88f79426b345c2e3
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2024-01-10 12:55:22 +0530

    Enemy added

---
+++ Assets/Scripts/Proto4/Prot4PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prot4PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody playerRb;
+    public GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed);
+    }
+}
